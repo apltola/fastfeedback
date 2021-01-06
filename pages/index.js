ec3e4d70c42e23f8cb2c8c3d54f25738acc6447f
allu <aleksijaakkojuhani@gmail.com>
2021-01-06 14:28:42 +0200

    fix

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import styles from '../styles/Home.module.css';
 
 export default function Home() {
   const auth = useAuth();
+  console.log('jee');
 
   return (
     <div className={styles.container}>
